26e1768ff4ac7e1008e514dc5258295e2ae685a2
Павел <mihailov.pashka@yandex.ru>
2026-05-25 23:10:21 +0300

    added log tracing

--- src/main/java/org/test/service/impl/TransferServiceImpl.java
+++ src/main/java/org/test/service/impl/TransferServiceImpl.java
@@ -1,6 +1,8 @@
 package org.test.service.impl;
 
 import javax.transaction.Transactional;
+import brave.Span;
+import brave.Tracer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.CacheEvict;
@@ -23,45 +25,64 @@ public class TransferServiceImpl implements TransferService {
     private final AccountRepository accountRepository;
     private final TransferMapper transferMapper;
     private final UserAccessService userAccessService;
+    private final Tracer tracer;
 
     @Override
     @Transactional
     @CacheEvict(value = "users", allEntries = true)
     public TransferResponseDto transfer(Long fromUserId, TransferRequest request) {
-        log.info("Старт перевода - от: {}, к: {}, сумма: {}",
-                fromUserId, request.getToUserId(), request.getValue());
+        Span span = tracer.nextSpan()
+                .name("money-transfer")
+                .tag("from.user.id", fromUserId.toString())
+                .tag("to.user.id", request.getToUserId().toString())
+                .tag("amount", request.getValue().toString())
+                .start();
 
-        userAccessService.checkUser(fromUserId);
+        try (Tracer.SpanInScope spanInScope = tracer.withSpanInScope(span)) {
+            log.info("Старт перевода - от: {}, к: {}, сумма: {}",
+                    fromUserId, request.getToUserId(), request.getValue());
 
-        validateTransfer(fromUserId, request);
+            userAccessService.checkUser(fromUserId);
 
-        Long firstUserId = fromUserId < request.getToUserId() ? fromUserId : request.getToUserId();
-        Long secondUserId = fromUserId < request.getToUserId() ? request.getToUserId() : fromUserId;
+            validateTransfer(fromUserId, request);
 
-        Account firstAccount = accountRepository.findByUserId(firstUserId)
-                .orElseThrow(() -> new ResourceNotFoundException("Аккаунт не найден"));
-        Account secondAccount = accountRepository.findByUserId(secondUserId)
-                .orElseThrow(() -> new ResourceNotFoundException("Аккаунт не найден"));
+            Long firstUserId = fromUserId < request.getToUserId() ? fromUserId : request.getToUserId();
+            Long secondUserId = fromUserId < request.getToUserId() ? request.getToUserId() : fromUserId;
 
-        Account fromAccount = fromUserId.equals(firstUserId) ? firstAccount : secondAccount;
-        Account toAccount = fromUserId.equals(firstUserId) ? secondAccount : firstAccount;
+            Account firstAccount = accountRepository.findByUserId(firstUserId)
+                    .orElseThrow(() -> new ResourceNotFoundException("Аккаунт не найден"));
+            Account secondAccount = accountRepository.findByUserId(secondUserId)
+                    .orElseThrow(() -> new ResourceNotFoundException("Аккаунт не найден"));
 
-        if (fromAccount.getBalance().compareTo(request.getValue()) < 0) {
-            throw new InsufficientFundsException(
-                    String.format("Недостаточно средств: баланс=%s, требуется=%s",
-                            fromAccount.getBalance(), request.getValue()));
+            Account fromAccount = fromUserId.equals(firstUserId) ? firstAccount : secondAccount;
+            Account toAccount = fromUserId.equals(firstUserId) ? secondAccount : firstAccount;
+
+            if (fromAccount.getBalance().compareTo(request.getValue()) < 0) {
+                span.tag("error", "insufficient_funds");
+                throw new InsufficientFundsException(
+                        String.format("Недостаточно средств: баланс=%s, требуется=%s",
+                                fromAccount.getBalance(), request.getValue()));
+            }
+
+            fromAccount.setBalance(fromAccount.getBalance().subtract(request.getValue()));
+            toAccount.setBalance(toAccount.getBalance().add(request.getValue()));
+
+            accountRepository.save(fromAccount);
+            accountRepository.save(toAccount);
+
+            span.tag("status", "success");
+
+            log.info("Перевод успешно завершён: транзакция от {} к {}, сумма {}",
+                    fromUserId, request.getToUserId(), request.getValue());
+
+            return transferMapper.toTransferResponseDto(fromUserId, request);
+        } catch (Exception e) {
+            span.error(e);
+            throw e;
+        } finally {
+            span.finish();
         }
 
-        fromAccount.setBalance(fromAccount.getBalance().subtract(request.getValue()));
-        toAccount.setBalance(toAccount.getBalance().add(request.getValue()));
-
-        accountRepository.save(fromAccount);
-        accountRepository.save(toAccount);
-
-        log.info("Перевод успешно завершён: транзакция от {} к {}, сумма {}",
-                fromUserId, request.getToUserId(), request.getValue());
-
-        return transferMapper.toTransferResponseDto(fromUserId, request);
     }
 
     private void validateTransfer(Long fromUserId, TransferRequest request) {
